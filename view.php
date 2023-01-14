4936b1bbb81e39fa7c1e306761006202ca8785f6
Asanka Lakmal Morawaka <almorawaka@gmail.com>
2023-01-14 23:25:34 +0530

    first commit via vS code

--- view.php
+++ view.php
@@ -19,12 +19,6 @@ include("auth.php");
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title> Job Card</title>
-
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0"> 
 <!-- <link rel="stylesheet" href="css/style.css" /> -->
     <!-- Required meta tags -->
     <!-- <meta charset="utf-8">
